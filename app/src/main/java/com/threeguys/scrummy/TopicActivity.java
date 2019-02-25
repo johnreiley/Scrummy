7b0eea617b74fd28379a9c64b99fdc7918c77ecb
Napler <pre15006@byui.edu>
2019-02-25 15:17:04 -0700

    Added Plus Button functionality

--- app/src/main/java/com/threeguys/scrummy/TopicActivity.java
+++ app/src/main/java/com/threeguys/scrummy/TopicActivity.java
@@ -1,8 +1,12 @@
 package com.threeguys.scrummy;
 
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class TopicActivity extends AppCompatActivity {
 
@@ -10,11 +14,30 @@ public class TopicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic);
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id._snackbarLayout);
+        linearLayout.setVisibility(View.INVISIBLE);
     }
 
     public void onClickVote(View view) {
     }
 
     public void onClickAddTopic(View view) {
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id._snackbarLayout);
+        if(linearLayout.getVisibility() == View.VISIBLE)
+        {
+            linearLayout.setVisibility(View.INVISIBLE);
+        }
+        if(linearLayout.getVisibility() == View.INVISIBLE)
+        {
+            linearLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onClickFinishInput(View view) {
+
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id._snackbarLayout);
+        linearLayout.setVisibility(View.INVISIBLE);
     }
 }
