45915a01fc522af1ca283abad0837e23bd2b1cf3
LouEvans <imbrettonsteiner@gmail.com>
2019-02-25 13:09:16 -0700

    Added Session.java and Topic.java as stubbed classes.

---
+++ app/src/main/java/com/threeguys/scrummy/Session.java
@@ -0,0 +1,17 @@
+package com.threeguys.scrummy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Session {
+
+    private List<Topic> topics;
+
+    Session(){
+        topics = new ArrayList<>();
+    }
+
+    public void sortByVote(){
+
+    }
+}
---
+++ app/src/main/java/com/threeguys/scrummy/Topic.java
@@ -0,0 +1,60 @@
+package com.threeguys.scrummy;
+
+public class Topic {
+
+    private String title;
+    private String actions;
+    private String username;
+    private Category category;
+    private int votes;
+
+    public enum Category {
+        GOOD,
+        NEUTRAL,
+        BAD
+    }
+
+    Topic() {
+        votes = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getActions() {
+        return actions;
+    }
+
+    public void setActions(String actions) {
+        this.actions = actions;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+}
