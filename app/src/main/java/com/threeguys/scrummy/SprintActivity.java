945ddd643ca3fe549e90926090ad7fcf3106997d
Napler <pre15006@byui.edu>
2019-02-25 14:17:17 -0700

    Topic Sprint Main added

---
+++ app/src/main/java/com/threeguys/scrummy/SprintActivity.java
@@ -0,0 +1,13 @@
+package com.threeguys.scrummy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sprint);
+    }
+}
--- app/src/main/java/com/threeguys/scrummy/TopicActivity.java
+++ app/src/main/java/com/threeguys/scrummy/TopicActivity.java
@@ -4,21 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class TopicActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_topic);
     }
 
-    public void onClickNew(View view) {
+    public void onClickVote(View view) {
     }
 
-    public void onClickContinue(View view) {
-    }
-
-    public void onClickLoad(View view) {
+    public void onClickAddTopic(View view) {
     }
 }
