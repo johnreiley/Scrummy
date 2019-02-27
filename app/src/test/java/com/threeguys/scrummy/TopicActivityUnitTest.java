728bde3222a3f647bb7bbb22189fa6458ad8d85e
Napler <pre15006@byui.edu>
2019-02-27 15:12:57 -0700

    Commented TopicActivity Unit Test Added

---
+++ app/src/test/java/com/threeguys/scrummy/TopicActivityUnitTest.java
@@ -0,0 +1,43 @@
+package com.threeguys.scrummy;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TopicActivityUnitTest {
+
+//    @Test
+//    public void layoutVisibilityTest() {
+//        TopicActivity ta = new TopicActivity();
+//        FloatingActionButton topicFAB = ta.findViewById(R.id._addTopicFloatingActionButton);
+//        LinearLayout snackLL = ta.findViewById(R.id._snackbarLayout);
+//
+//        ta.onClickAddTopic(topicFAB);
+//
+//        assertEquals(View.VISIBLE, snackLL.getVisibility());
+//    }
+
+//    @Test
+//    public void addTopicTest() {
+//        String topic = "TOPIC";
+//        String name = "NAME";
+//
+//        TopicActivity ta = new TopicActivity();
+//        LinearLayout snackLL = ta.findViewById(R.id._snackbarLayout);
+//        Button finish = snackLL.findViewById(R.id._addTopicButton);
+//        EditText topicET = snackLL.findViewById(R.id._topicInputEditText);
+//        topicET.setText(topic);
+//        EditText nameET = snackLL.findViewById(R.id._topicInputEditText);
+//        topicET.setText(name);
+//
+//        finish.callOnClick();
+//
+//        assertEquals(topic, ta.getSession().getTopics().get(0).getTitle());
+//        assertEquals(name, ta.getSession().getTopics().get(0).getUsername());
+//    }
+
+}
