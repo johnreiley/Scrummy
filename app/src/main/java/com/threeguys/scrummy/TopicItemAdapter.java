13e5c245e86da16565809b497163999b777d37aa
LouEvans <imbrettonsteiner@gmail.com>
2019-03-04 14:40:04 -0700

    Created Adapter and Item for the Topic Activity and Fixed compile error in load_session_item.xml

---
+++ app/src/main/java/com/threeguys/scrummy/TopicItemAdapter.java
@@ -0,0 +1,104 @@
+package com.threeguys.scrummy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TopicItemAdapter extends BaseExpandableListAdapter{
+
+    private Context context;
+
+    // group titles
+    private List<String> categories;
+
+    // child data
+    private HashMap<String, List<Topic>> topics;
+
+    public TopicItemAdapter(Context context, HashMap<String, List<Topic>> topics, List<String> categories) {
+        this.context = context;
+        this.topics = topics;
+        this.categories = categories;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return categories.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return topics.size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return categories.get(groupPosition);
+    }
+
+    @Override
+    public Topic getChild(int groupPosition, int childPosition) {
+        return this.topics.get(this.categories.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.category_item, null);
+        }
+
+        TextView textViewGroup = convertView
+                .findViewById(R.id._categoryTextView);
+        textViewGroup.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        Topic topic = getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.vote_item, null);
+        }
+
+        TextView titleTextView = convertView.findViewById(R.id._topicItemTitleTextView);
+        TextView usernameTextView = convertView.findViewById(R.id._topicItemUsernameTextView);
+
+        titleTextView.setText(topic.getTitle());
+        usernameTextView.setText(topic.getUsername());
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
