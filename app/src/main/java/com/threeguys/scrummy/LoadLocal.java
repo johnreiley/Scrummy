64cd61d8e34f4a99f313c8e02356fb81c9e46d38
NathanPrestwich <sirnapsalot@gmail.com>
2019-03-12 14:20:40 -0600

    Added date to session on quit

--- app/src/main/java/com/threeguys/scrummy/LoadLocal.java
+++ app/src/main/java/com/threeguys/scrummy/LoadLocal.java
@@ -1,9 +1,25 @@
 package com.threeguys.scrummy;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoadLocal implements Load {
 
     @Override
-    public Session load() {
-        return null;
+    public List<Session> load(Context context) {
+        List<Session> sessions = new ArrayList<>();
+
+        Gson gson = new Gson();
+        SharedPreferences sp = context.getSharedPreferences(MainActivity.SAVE_PREF, Context.MODE_PRIVATE);
+        String saves = sp.toString();
+
+        sessions.add(gson.fromJson(saves, Session.class)); // TODO have a class to hold a list of sessions
+
+        return sessions;
     }
 }
