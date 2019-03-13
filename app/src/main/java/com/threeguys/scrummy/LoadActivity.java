cd9dfa3af4f5f87901742c399904a67145b633ec
Napler <pre15006@byui.edu>
2019-03-13 13:49:26 -0600

    Loading implemented

--- app/src/main/java/com/threeguys/scrummy/LoadActivity.java
+++ app/src/main/java/com/threeguys/scrummy/LoadActivity.java
@@ -33,60 +33,61 @@ public class LoadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_load);
         Log.i(LOADACTIVITY_TAG, "LoadActivity Started");
 
-        sessions = new ArrayList<>();
+        Load loader = new LoadLocal();
+        sessions = loader.load(this).list;
 
         //TEST Dummy code
-        Topic t1 = new Topic();
-        t1.setTitle("Good Test1");
-        t1.setUsername("Bretton");
-        t1.setCategory(Topic.Category.GOOD);
-
-        Topic t2 = new Topic();
-        t2.setTitle("Neutral Test1");
-        t2.setUsername("Bretton");
-        t2.setCategory(Topic.Category.NEUTRAL);
-        t2.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
-
-        Topic t3 = new Topic();
-        t3.setTitle("Bad Test1");
-        t3.setUsername("Bretton");
-        t3.setCategory(Topic.Category.BAD);
-        t3.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
-
-        Session session1 = new Session();
-        session1.setTitle("First Sprint Meeting");
-        session1.setDate("Yesterday");
-        session1.addTopic(t1);
-        session1.addTopic(t2);
-        session1.addTopic(t3);
-
-        Topic t4 = new Topic();
-        t4.setTitle("Good Test2");
-        t4.setUsername("Bretton");
-        t4.setCategory(Topic.Category.GOOD);
-        t4.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
-
-        Topic t5 = new Topic();
-        t5.setTitle("Neutral Test2");
-        t5.setUsername("Bretton");
-        t5.setCategory(Topic.Category.NEUTRAL);
-        t5.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
-
-        Topic t6 = new Topic();
-        t6.setTitle("Bad Test2");
-        t6.setUsername("Bretton");
-        t6.setCategory(Topic.Category.BAD);
-        t6.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
-
-        Session session2 = new Session();
-        session2.setTitle("Second Sprint Meeting - Follow Up");
-        session2.setDate("Today");
-        session2.addTopic(t4);
-        session2.addTopic(t5);
-        session2.addTopic(t6);
-
-        sessions.add(session1);
-        sessions.add(session2);
+//        Topic t1 = new Topic();
+//        t1.setTitle("Good Test1");
+//        t1.setUsername("Bretton");
+//        t1.setCategory(Topic.Category.GOOD);
+//
+//        Topic t2 = new Topic();
+//        t2.setTitle("Neutral Test1");
+//        t2.setUsername("Bretton");
+//        t2.setCategory(Topic.Category.NEUTRAL);
+//        t2.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
+//
+//        Topic t3 = new Topic();
+//        t3.setTitle("Bad Test1");
+//        t3.setUsername("Bretton");
+//        t3.setCategory(Topic.Category.BAD);
+//        t3.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
+//
+//        Session session1 = new Session();
+//        session1.setTitle("First Sprint Meeting");
+//        session1.setDate("Yesterday");
+//        session1.addTopic(t1);
+//        session1.addTopic(t2);
+//        session1.addTopic(t3);
+//
+//        Topic t4 = new Topic();
+//        t4.setTitle("Good Test2");
+//        t4.setUsername("Bretton");
+//        t4.setCategory(Topic.Category.GOOD);
+//        t4.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
+//
+//        Topic t5 = new Topic();
+//        t5.setTitle("Neutral Test2");
+//        t5.setUsername("Bretton");
+//        t5.setCategory(Topic.Category.NEUTRAL);
+//        t5.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
+//
+//        Topic t6 = new Topic();
+//        t6.setTitle("Bad Test2");
+//        t6.setUsername("Bretton");
+//        t6.setCategory(Topic.Category.BAD);
+//        t6.setActions("Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action Action");
+//
+//        Session session2 = new Session();
+//        session2.setTitle("Second Sprint Meeting - Follow Up");
+//        session2.setDate("Today");
+//        session2.addTopic(t4);
+//        session2.addTopic(t5);
+//        session2.addTopic(t6);
+//
+//        sessions.add(session1);
+//        sessions.add(session2);
         //End dummy code
 
         adapter = new LoadSessionItemAdapter(this, sessions);
