0965aaa5d92356b66f88fb23d764d66e75dabdca
Napler <pre15006@byui.edu>
2019-03-15 13:33:29 -0600

    Loaded List is ordered by date

--- app/src/main/java/com/threeguys/scrummy/LoadActivity.java
+++ app/src/main/java/com/threeguys/scrummy/LoadActivity.java
@@ -53,7 +53,7 @@ public class LoadActivity extends AppCompatActivity {
             Collections.sort(sessions, new Comparator<Session>() {
                 @Override
                 public int compare(Session o1, Session o2) {
-                    return o1.getDate().compareTo(o2.getDate());
+                    return o2.getDate().compareTo(o1.getDate());
                 }
             });
     }
