d2813ff2bf274f691a2b94aa99fa9ac313a7e199
LouEvans <imbrettonsteiner@gmail.com>
2019-03-29 12:42:53 -0600

    Added About Activity and updated all Activity titles

--- app/src/main/java/com/threeguys/scrummy/SettingsActivity.java
+++ app/src/main/java/com/threeguys/scrummy/SettingsActivity.java
@@ -239,7 +239,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+            bindPreferenceSummaryToValue(findPreference("load_method"));
+            //bindPreferenceSummaryToValue(findPreference("save_cloud"));
+            //bindPreferenceSummaryToValue(findPreference("save_local"));
         }
 
         @Override
