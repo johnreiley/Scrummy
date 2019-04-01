2a390eb3d0767359b7f26c703345171327d065e0
LouEvans <imbrettonsteiner@gmail.com>
2019-04-01 13:10:45 -0600

    Updated app colors and Icon. Added sign out option to main activity menu.

--- app/src/main/java/com/threeguys/scrummy/MainActivity.java
+++ app/src/main/java/com/threeguys/scrummy/MainActivity.java
@@ -17,8 +17,10 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -77,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id._settingsMenuItem:
                 startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                return true;
+            case R.id._signOutMenuItem:
+                onClickSignOut(null);
                 return true;
         }
 
