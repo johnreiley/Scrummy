39ac14697bc859a33daf8b55284de3f3cda37869
NathanPrestwich <sirnapsalot@gmail.com>
2019-04-05 20:31:00 -0600

    Added UI test

---
+++ app/src/test/java/com/threeguys/scrummy/UiTesting.java
@@ -0,0 +1,56 @@
+package com.threeguys.scrummy;
+
+public class UiTesting {
+    /*
+     * New Account
+     *
+     * Test login with wrong password
+     *  -verify that there is a toast warning the user that the password is wrong.
+     *
+     * Login
+     *  -verify that only two buttons are visible on MainActivity
+     *  -verify that the kebab menu works
+     *      -verify that all options pop-up and lead the user to the corresponding activities
+     *
+     * Click new
+     *  -verify that the toast pops up when there is no title inputted
+     *  -verify that the TopicActivity starts when a title is inputted
+     *
+     * Click the back arrow
+     *  -verify that the continue/join button now shows up
+     *
+     * Click the continue button
+     *  -verify that the correct title appears
+     *  -verify that the vote button pops up a toast asking for a topic to be added
+     *  -verify that a topic can be added
+     *      -verify that the toasts popup when there is no text inputted
+     *  -verify that the edit topic kebab menu works
+     *  -verify that topics can be deleted
+     *
+     * Add a few topics and click the vote button
+     *  -verify that all the topics display correctly
+     *  -verify that the plus and minus buttons work
+     *
+     * Click start
+     *  -verify that the most voted topic displays first
+     *  -verify that actions can be inputted
+     *  -wait 5 minutes and verify that the alarm goes off
+     *  -verify that the time can be edited
+     *  -verify that the pause, back, forward, and mute buttons all work.
+     *
+     * Click next topic
+     *  -verify that the previous topic button shows up and works
+     *  -on the last topic (verify that the next button becomes save and quit).
+     *
+     * Click save and quit
+     *  -verify that the mainActivity starts
+     *
+     * Click Load
+     *  -verify that the session saved
+     *  -verify that you can switch between cloud and local
+     *
+     * TRY THE PREVIOUS STEPS WITH CLOUD SAVING, LOCAL SAVING, NO INTERNET CONNECTION,
+     * AND WITH OTHER DEVICES CONNECTED IN ALL DIFFERENT COMBINATIONS.
+     *
+     */
+}
